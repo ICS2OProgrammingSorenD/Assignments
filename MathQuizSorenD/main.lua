6b0fb437ecb69bbdaf779cda4c64e88f3a3dab78
Soren15 <43419141+Soren15@users.noreply.github.com>
2018-11-05 13:49:04 -0500

    added company logo animation

--- MathQuizSorenD/main.lua
+++ MathQuizSorenD/main.lua
@@ -1,4 +1,4 @@
------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 --Title: Math quiz
 -- Name:Soren Drew 
 --Course:ICS2O
@@ -17,10 +17,10 @@ local correctSound = audio.loadSound( "Sounds/correctSound.mp3" )
 local correctSoundChannel
 
 --incorrect sound
-local incorrectSound = audio.loadSound( "Sounds/wrongSound.mp3" )
+local incorrectSound = audio.loadSound( "Sounds/buzzer.mp3" )
 local incorrectSoundChannel
 
-local gameOverSound = audio.loadSound( "Sounds/booSound.mp3" )
+local gameOverSound = audio.loadSound( "Sounds/crowdBoo.mp3" )
 local gameOverSoundChannel
 -----------------------------------------------------------------------------------------
 --LOCAL VARIABLES
@@ -31,6 +31,7 @@ local questionObject
 
 local correctObject
 local incorrectObject
+local showCorrectObject
 
 local numericField
 
@@ -39,6 +40,7 @@ local randomNumber2
 local userAnswer
 local correctAnswer
 local randomOperator
+local temp
 
 local numberPoints = 0
 local pointsObject
@@ -61,159 +63,202 @@ local youWin
 -----------------------------------------------------------------------------------------
 
 local function AskQuestion()
-	--generate random operator (number betwen 1 and 3)
-	randomOperator = math.random(1, 4)
+    --generate random operator (number betwen 1 and 3)
+    randomOperator = math.random(1, 5)
 
-	--generate 2 random numbers between 1 and 10 
-	randomNumber1 = math.random(0, 10)
-	randomNumber2 = math.random(0, 10)
+    if (randomOperator == 1) then
 
-	if (randomOperator == 1) then
+        --generate 2 random numbers between 1 and 10 
+        randomNumber1Ad = math.random(0, 20)
+        randomNumber2Ad = math.random(0, 20)
 
-		correctAnswer = randomNumber1  +  randomNumber2 
+        correctAnswer = randomNumber1Ad  +  randomNumber2Ad 
 
-		-- create question in text object
-	    questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+        -- create question in text object
+        questionObject.text = randomNumber1Ad .. " + " .. randomNumber2Ad .. " = "
 
-	elseif (randomOperator == 2) then
-		correctAnswer = randomNumber1  -  randomNumber2  
+    elseif (randomOperator == 2) then
+         --generate 2 random numbers between 1 and 10 
+            randomNumber1Sub = math.random(0, 20)
+            randomNumber2Sub = math.random(0, 20)
+         --create the problem
+        if ( randomNumber1Sub  <  randomNumber2Sub ) then
 
-		--create question in text object
-		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
-	
+            --change the numbers so there is no negatives
+            randomNumber1 = temp
+            randomNumber2 = randomNumber1
+            temp = randomNumber2
 
-	elseif (randomOperator == 3) then
+            --create the equation
+            correctAnswer = randomNumber1Sub - randomNumber2Sub
 
-		correctAnswer = randomNumber1  *  randomNumber2  
+            --create the question in text object
+            questionObject.text = randomNumber1Sub .. " - " .. randomNumber2Sub .. " = "
+    
 
-		--create question in text object
-		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+      elseif ( randomNumber1 > randomNumber2 ) then
+    
+        --create the equation
+        correctAnswer = randomNumber1Sub - randomNumber2Sub
+        --create question in text object
+        questionObject.text = randomNumber1Sub .. " - " .. randomNumber2Sub .. " = "
+        end
+        
+    
+    elseif (randomOperator == 3) then
 
-	elseif (randomOperator == 4 ) then 
+        --generate 2 random numbers between 1 and 10 
+        randomNumber1Mul = math.random(0, 10)
+        randomNumber2Mul = math.random(0, 10)
 
-		correctAnswer = randomNumber1 / randomNumber2
+        --create the equation
+        correctAnswer = randomNumber1Mul  *  randomNumber2Mul  
 
-		--create question in text object
-		questionObject.text = randomNumber1 ..  " / " .. randomNumber2 .. " = "
+        --create question in text object
+        questionObject.text = randomNumber1Mul .. " * " .. randomNumber2Mul .. " = "
 
-	end
+    elseif (randomOperator == 4 ) then 
+
+        --generate 2 random numbers between 1 and 10 
+        randomNumber1Div = math.random(0, 100)
+        randomNumber2Div = math.random(0, 100)
+
+        --create the equation
+        correctAnswer = randomNumber1Div / randomNumber2Div
+
+        --round the number
+        math.round( correctAnswer )
+
+        --create question in text object
+        questionObject.text = randomNumber1Div ..  " ÷ " .. randomNumber2Div .. " = "
+    end
 end
+
 
 local function GameOver()
-	if (lives == 0) then
-		heart3.isVisible = false
-		heart2.isVisible = false
-		heart1.isVisible = false
-		gameOver = display.newImageRect("Images/gameOver2.png", 1028, 756)
-		gameOver.x = display.contentWidth/2
-		gameOver.y = display.contentHeight/2
-		gameOverSoundChannel = audio.play(gameOverSound)
-	else 
-		AskQuestion()
-	end
-end
 
-local function YouWin()
-	if (numberPoints == 5) then
-		display.newImage("Images/trophy.png", 1024, 756)
-	end
-end
+    if (lives == 0) then
+        heart3.isVisible = false
+        heart2.isVisible = false
+        heart1.isVisible = false
+        --display the image
+        gameOver = display.newImageRect("Images/gameOver2.png", 1028, 756)
+        --display the image over the entire screen
+        gameOver.x = display.contentWidth/2
+        gameOver.y = display.contentHeight/2
+        --play the gameover sound
+        gameOverSoundChannel = audio.play(gameOverSound)
 
+    end
+end
 
 local function UpdateHearts()
 
-	if (lives == 2 ) then 
-		heart3.isVisible = false
-	elseif (lives == 1) then
-		heart2.isVisible = false
-	elseif (lives == 0) then
-	GameOver()
-	end
+    if (lives == 3) then
+        heart4.isVisible = false
+    elseif (lives == 2 ) then 
+        heart3.isVisible = false
+    elseif (lives == 1) then
+        heart2.isVisible = false
+    elseif (lives == 0) then
+    GameOver()
+    end
 end
-
-
-
 
 
 local function UpdateTime()
 
-	--decrement the number of seconds
-	secondsLeft = secondsLeft - 1
+    --decrement the number of seconds
+    secondsLeft = secondsLeft - 1
 
-	--display the number of seconds left in the clock object
-	clockText.text = "Time: \n"  ..    secondsLeft
+    --display the number of seconds left in the clock object
+    clockText.text = "Time: \n"  ..    secondsLeft
 
-	if (secondsLeft == 0 ) then
-		--reset the number of seconds left
-		secondsLeft = totalSeconds
-		-- decrease life
-		lives = lives - 1 
-		-- update the hearts
-		UpdateHearts()
-		-- call game over or ask another question
-		GameOver()
-	
-	end
-		
+    if (secondsLeft == 0 ) then
+        --reset the number of seconds left
+        secondsLeft = totalSeconds
+        -- decrease life
+        lives = lives - 1 
+        -- update the hearts
+        UpdateHearts()
+        -- call game over or ask another question
+        GameOver()
+    end       
 end
 
-
-
-				
+                
 --function that calls the timer
 local function StartTimer()
-	--create a countdown timer that loops infintely
-	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+    --create a countdown timer that loops infintely
+    countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
 end
-
-
 
 
 local function HideCorrect()
-	correctObject.isVisible = false
-	AskQuestion() 
+    correctObject.isVisible = false
+    AskQuestion() 
 end
 
 local function HideIncorrect()
-	incorrectObject.isVisible = false
-	AskQuestion()
+    incorrectObject.isVisible = false
+    AskQuestion()
 end
- 
-local function NumericFieldListener( event )	
 
-	--user begins editing "numericfield"
-	if (event.phase == "began") then
 
-		--clear text field
-		event.target.text = ""
+local function NumericFieldListener( event )    
 
-	elseif (event.phase == "submitted") then 
-		--when the answer is submitted (enter key is pressed) set user input to user's answer 
-		userAnswer = tonumber(event.target.text)
-		print(userAnswer)
+    --user begins editing "numericfield"
+    if (event.phase == "began") then
 
-		--if the users answer and the correct answer are the same:
-		if (userAnswer == correctAnswer) then 
-			correctObject.isVisible = true 
-			correctSoundChannel = audio.play(correctSound)
-			numberPoints = numberPoints + 1
-			pointsObject.text = "Points:" .. numberPoints
-			timer.performWithDelay(1000, HideCorrect)
-		
-		else 
-			correctObject.isVisible = false 
-			incorrectObject.isVisible = true
-			incorrectSoundChannel = audio.play(incorrectSound)
-			timer.performWithDelay(1000, HideIncorrect) 
-			lives = lives - 1 
-			UpdateHearts()
-			
-		end
+        --clear text field
+        event.target.text = ""
 
-		event.target.text = ""
+    elseif (event.phase == "submitted") then 
+        --when the answer is submitted (enter key is pressed) set user input to user's answer 
+        userAnswer = tonumber(event.target.text)
+        print(userAnswer)
+
+        --if the users answer and the correct answer are the same:
+        if (userAnswer == correctAnswer) then 
+            correctObject.isVisible = true 
+            --play correct sound
+            correctSoundChannel = audio.play(correctSound)
+            -- add a point
+            numberPoints = numberPoints + 1
+            --display the points
+            pointsObject.text = "Points:" .. numberPoints
+            timer.performWithDelay(1000, HideCorrect)
+        else 
+            correctObject.isVisible = false 
+            incorrectObject.isVisible = true
+            --play incorrect sound
+            incorrectSoundChannel = audio.play(incorrectSound)
+            timer.performWithDelay(1000, HideIncorrect) 
+            --decrease a life
+            lives = lives - 1 
+            --show correct answer
+            --showCorrectObject.text = "Correct Answer: " .. correctAnswer
+            UpdateHearts()
+        end
+
+        event.target.text = ""
+    end 
+end
+
+local function YouWin()
+    
+    if (numberPoints == 5) then
+        --show image of trophy
+        display.newImage("Images/trophy.png", 1024, 756)
+        --display a text saying you win
+        display.newText("You Win", 400, 600, nil, 150)
+        --create the applause sound
+        local applauseSound = audio.loadSound( "Sounds/applause.mp3" )
+        local applauseSoundChannel
+        --call the applause sound
+        applauseSoundChannel = audio.play(applauseSound)
     end
 end
-  
 
 
 -----------------------------------------------------------------------------------------
@@ -221,15 +266,15 @@ end
 -----------------------------------------------------------------------------------------
 
 --create the lives to display on the creen 
-heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1 = display.newImageRect("Images/Heart.png", 100, 100)
 heart1.x = display.contentWidth * 7 / 8
 heart1.y = display.contentHeight * 1 / 7
 
-heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2 = display.newImageRect("Images/Heart.png", 100, 100)
 heart2.x = display.contentWidth * 6 / 8
 heart2.y = display.contentHeight * 1 / 7
 
-heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3 = display.newImageRect("Images/Heart.png", 100, 100)
 heart3.x = display.contentWidth * 5 / 8
 heart3.y = display.contentHeight * 1 / 7
 
@@ -257,18 +302,22 @@ numericField.inputType = "number"
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener )
 
+--display the points 
 pointsObject = display.newText ("Points:" .. numberPoints, 100, 100, nil, 50)
 pointsObject:setTextColor(1, 1, 1)
 
+--display the timer
 clockText = display.newText ("Time: \n"  ..  secondsLeft, 100, 500, nil, 50)
 clockText:setTextColor(1, 1, 1)
 
-
-
+--display the correct answer if incorrect
+--showCorrectObject = display.newText ("Correct: \n" .. correctAnswer, 300, 100, nil, 50)
+--showCorrectObject:setTextColor(1, 1, 1)
 -----------------------------------------------------------------------------------------
 --FUNCTION CALLS 
 -----------------------------------------------------------------------------------------
 
 -- call the function to ask the question 
 AskQuestion()
+
 StartTimer()
